1c7ab55089bb876845fbcea5e2ccc3331e000adf
TakrayoutMohamed <takrayoutm@gmail.com>
2023-10-24 21:40:21 +0100

    change something in the main programe

--- modul00/ex01/main.cpp
+++ modul00/ex01/main.cpp
@@ -6,11 +6,32 @@
 
 int	main(void)
 {
-	PhoneBook phoneBook;
+	PhoneBook	phoneBook;
+	std::string	command;
+	int			id;
 
-	phoneBook.addContact(0);
-	phoneBook.addContact(1);
-	phoneBook.printPhoneBook();
+	id = 0;
+	do {
+		std::cout << "Enter :" << std::endl;
+		std::cout << "ADD: to save a new contact" << std::endl;
+		std::cout << "SEARCH: display a specific contact" << std::endl;
+		std::cout << "EXIT: to exit the program" << std::endl;
+		std::getline(std::cin, command);
+		if (command.compare("ADD") == 0)
+		{
+			phoneBook.addContact(id % 8);//still need to work on this add contact 
+			id++;
+		}
+		else if (command.compare("SEARCH") == 0)
+		{ 
+			phoneBook.printPhoneBook();
+		}
+		else if (command.compare("EXIT") == 0)
+		{
+			break ;
+		}
+
+	} while (1);
 
 	return (0);
 }
