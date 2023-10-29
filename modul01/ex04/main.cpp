a1c8da6a326cd2fcc9346e7ce75e9fd9cd26b9fd
TakrayoutMohamed <takrayoutm@gmail.com>
2023-10-30 00:50:06 +0100

    sss

--- modul01/ex04/main.cpp
+++ modul01/ex04/main.cpp
@@ -1,86 +1,81 @@
-// #include "./File.hpp"
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 
-// void	write_to_file(std::ofstream file, std::string text)
-// {
-
-// }
-
-// std::ofstream	open_file(std::string fileToOpen)
-// {
-// 	std::ofstream	file;
-
-// 	file.open(fileToOpen);
-// 	return (file);
-// }
-std::string	replace_text(std::string line, char *str1, char *str2)
+std::string	replace_text(std::string line, std::string toReplace, std::string newStr)
 {
 	std::string newLine;
-	std::size_t	found;
+	// std::size_t	found;
 	int			i;
 
 
 	i = 0;
-	found = line.find(str1);
-	// if (found != std::string::npos)
 	while (line[i] != '\0')
 	{
-		newLine.append(line[i]);
+		if (line.compare(i, toReplace.length(), toReplace) == 0)
+		{
+			newLine.append(newStr);
+			i = i + toReplace.length();
+			continue ;
+		}
+		else
+			newLine.append(line[i]+"");
+		i++;
 	}
+	return (newLine);
 }
 
-void	write_to_file(std::ofstream newFile, const char * line)
-{
-	newFile << line;
-	newFile << std::endl;
-	return ;
-}
+// void	write_to_file(std::ofstream newFile, std::string line)
+// {
+// 	newFile << line;
+// 	newFile << std::endl;
+// 	return ;
+// }
 
 int	main(int argc, char **argv)
 {
-	std::string	fileName;
-	std::string	stringToReplace;
-	std::string	stringReplace;
-	std::string	line;
-	std::ofstream	newFile;
-	std::ifstream	currentfile;
-
 	if (argc != 4)
 	{
 		std::cout << "Error : ";
 		std::cout << "./no_sed filePath/fileName word new_word"<< std::endl;
-		return (EXIT_FAILURE);
+		return (1);
 	}
-	fileName = argv[1];
-	stringToReplace = argv[2];
-	stringReplace = argv[3];
+	std::string	fileName = argv[1];
+	std::string	strToReplace = argv[2];
+	std::string	strReplace = argv[3];
+	std::string	line;
+	std::ifstream	currentFile(argv[1]);
+	std::ofstream	newFile("ooook.replace");
+
 	// std::cout << "file name :"<< fileName<< std::endl;
-	// std::cout << "string to replace :"<< stringToReplace<< std::endl;
-	// std::cout << "string replace :"<< stringReplace<< std::endl;
-	currentfile.open(fileName);//here we open the file to read from
-	if (!currentfile.is_open())
-	{
-		std::cout << "Error: could not open the file " << fileName;
-		std::cout << std::endl;
-		return (EXIT_FAILURE);
-	}
-	newFile.open(fileName + ".replace");
-	if (!newFile.is_open())
-	{
-		std::cout << "Error: could not create new file: " << fileName << ".replace";
-		std::cout << std::endl;
-		return (EXIT_FAILURE);
-	}
+	// std::cout << "string to replace :"<< strToReplace<< std::endl;
+	// std::cout << "string replace :"<< strReplace<< std::endl;
+	// currentfile.open(fileName);//here we open the file to read from
+	// if (!currentfile.is_open())
+	// {
+	// 	std::cout << "Error: could not open the file " << fileName;
+	// 	std::cout << std::endl;
+	// 	return (EXIT_FAILURE);
+	// }
+	// newFile.open(fileName + ".replace");
+	// if (!newFile.is_open())
+	// {
+	// 	std::cout << "Error: could not create new file: " << fileName << ".replace";
+	// 	std::cout << std::endl;
+	// 	return (EXIT_FAILURE);
+	// }
 	while (1)
 	{
-		std::getline(currentfile, line);//read one line from the file and put it into string line
-		if (currentfile.eof())
+		std::getline(currentFile, line);//read one line from the file and put it into string line
+		if (currentFile.eof())
 			break ;
 		// std::cout << line << std::endl;
-		std::cout << line.find(stringReplace);
-		break;
+		newFile << replace_text(line, strToReplace, strReplace);
+		newFile << std::endl;
+		// write_to_file(newFile, replace_text(line, strToReplace, strReplace));
+		// std::cout << line.find(strReplace);
+		// break;
 		
 	}
 
