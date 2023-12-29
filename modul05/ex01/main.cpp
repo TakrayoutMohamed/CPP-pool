296ea1eabf8bf1b2cd51f83f059ff9332203b99e
TakrayoutMohamed <takrayoutm@gmail.com>
2023-12-29 13:48:46 +0100

    main

--- modul05/ex01/main.cpp
+++ modul05/ex01/main.cpp
@@ -4,11 +4,11 @@ int main(void)
 {
 	try
     {
-        Bureaucrat obj("Counselor", 1);
+        Bureaucrat obj("Counselor", 3);
         Form form("Judge", 2, 50);
         std::cout << obj << std::endl;
         std::cout << form << std::endl;
-        form.beSigned(obj);
+        // form.beSigned(obj);
         obj.signForm(form);
     }
     catch(std::exception &e)
