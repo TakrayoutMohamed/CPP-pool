72da2471ca81f37465b712161b3b8dafdc06a109
TakrayoutMohamed <takrayoutm@gmail.com>
2024-01-03 15:00:51 +0100

    add tests to main to test all the classes

--- modul05/ex02/main.cpp
+++ modul05/ex02/main.cpp
@@ -1,19 +1,42 @@
 #include "./Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 int main(void)
 {
 	try
-    {
-        Bureaucrat obj("Counselor", 3);
-        ShrubberyCreationForm shrubbery("Home");
-        std::cout << obj << std::endl;
-        shrubbery.beSigned(obj);
-        shrubbery.execute(obj);
-    }
-    catch(std::exception &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
+	{
+		std::cout << "\n------------------*** CONSTRUCTORS ***-------------------\n" << std::endl;
+		
+		Bureaucrat Counselor("Counselor", 20);
+		
+		PresidentialPardonForm ppf("Bandit");
+		RobotomyRequestForm	rrf("drivingLicence");
+		ShrubberyCreationForm scf("home");
+
+		std::cout << "\n--------------*** FORMS DEMONSTRATION ***---------------\n" << std::endl;
+		
+		ppf.beSigned(Counselor);
+		ppf.execute(Counselor);
+		rrf.beSigned(Counselor);
+		rrf.execute(Counselor);
+		scf.beSigned(Counselor);
+		scf.execute(Counselor);
+		
+		std::cout << "\n-----------*** BUREAUCRAT FORMS EXECUTION ***-----------\n" << std::endl;
+		
+		AForm *form;
+		
+		form = &rrf;
+		form->beSigned(Counselor);
+		Counselor.executeForm(*form);
+		
+		std::cout << "\n------------------*** DESTRUCTORS ***-------------------\n" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	return (0);
 }
