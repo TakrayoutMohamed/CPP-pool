17ca77a7420738cd3321b0ea8a8227d7f03ae65c
TakrayoutMohamed <takrayoutm@gmail.com>
2024-01-03 14:40:01 +0100

    add RobotomyRequest and add it to makefile

--- modul05/ex02/RobotomyRequestForm.hpp
+++ modul05/ex02/RobotomyRequestForm.hpp
@@ -1,17 +1,24 @@
 #ifndef ROBOTOMYREQUESTFORM_HPP
 # define ROBOTOMYREQUESTFORM_HPP
 # include "./AForm.hpp"
+#include <cstdlib>
 
 class RobotomyRequestForm : public AForm
 {
 	private:
-		/* data */
+		std::string _target;
 	public:
 		RobotomyRequestForm(/* args */);
 		~RobotomyRequestForm();
 		RobotomyRequestForm(const RobotomyRequestForm &);
-		const RobotomyRequestForm &operator=(const RobotomyRequestForm &) const;
+		const RobotomyRequestForm &operator=(const RobotomyRequestForm &);
 		RobotomyRequestForm(const std::string &);
+		/*getters*/
+		std::string getTarget(void) const;
+		
+		/*member functions*/
+		void	robotomyRequestAction(void) const;
+		void execute(Bureaucrat const & executor) const;
 };
 
 #endif
