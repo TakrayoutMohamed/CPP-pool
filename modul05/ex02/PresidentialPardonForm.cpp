355a1ecce7d1d0897e93e09e1c75464167a624ed
TakrayoutMohamed <takrayoutm@gmail.com>
2024-01-03 14:18:38 +0100

    add the presidentialPardonForm.{hpp, cpp} , add the file name to makefile

--- modul05/ex02/PresidentialPardonForm.cpp
+++ modul05/ex02/PresidentialPardonForm.cpp
@@ -1,9 +1,65 @@
 #include "./PresidentialPardonForm.hpp"
 
-PresidentialPardonForm::PresidentialPardonForm(/* args */)
+/*default constructor*/
+PresidentialPardonForm::PresidentialPardonForm(/* args */) : AForm("defaultAFormPrisPard", 25, 5)
 {
+	_target = "defaultpresidentialPardon";
+	std::cout << "Default Constructor for the presidentialPardon target named {"<< getTarget() <<"} has been called!"<< std::endl;
 }
 
+/*paramitrized constructor with string*/
+PresidentialPardonForm::PresidentialPardonForm(const std::string &target) : AForm("defaultAFormPrisPard", 25, 5)
+{
+	_target = target;
+	std::cout << "paramitrized Constructor for the presidentialPardon target named {"<< getTarget() <<"} has been called!"<< std::endl;
+}
+
+/*copy constructor*/
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &obj) : AForm(obj.getName(), obj.getGradeToSign(), obj.getGradeToExecute())
+{
+	if (this != &obj)
+	{
+		*this = obj;
+	}
+	std::cout << "Copy Constructor for the presidentialPardon target named {"<< obj.getTarget() << "} has been called!"<< std::endl;
+}
+
+
+/*copy assignment operator*/
+const PresidentialPardonForm &PresidentialPardonForm::operator=(const PresidentialPardonForm &obj)
+{
+	std::cout << "Copy assignment operator for the presidentialPardon target named {"<< obj.getTarget() << "} has been called!"<< std::endl;
+	if (this != &obj)
+	{
+		this->_target = obj.getTarget();
+	}
+	return (*this);
+}
+
+/*destructor*/
 PresidentialPardonForm::~PresidentialPardonForm()
 {
+	std::cout << "Destructor for the presidentialPardon target named {"<< getTarget() << "}  has been called!"<< std::endl;
+}
+
+/*getters*/
+
+std::string PresidentialPardonForm::getTarget(void) const
+{
+	return (this->_target);
+}
+
+/*member functions */
+
+void	PresidentialPardonForm::presidentialPardonAction(void) const
+{
+
+	std::cout << getTarget() <<  " has been pardoned by Zaphod Beeblerox." << std::endl;
+}
+
+void PresidentialPardonForm::execute(Bureaucrat const & executor) const
+{
+    (void) executor;
+    presidentialPardonAction();
+	
 }
--- modul05/ex02/PresidentialPardonForm.hpp
+++ modul05/ex02/PresidentialPardonForm.hpp
@@ -5,10 +5,19 @@
 class PresidentialPardonForm : public AForm
 {
 	private:
-		/* data */
+		std::string _target;
 	public:
 		PresidentialPardonForm(/* args */);
 		~PresidentialPardonForm();
+		PresidentialPardonForm(const PresidentialPardonForm &);
+		const PresidentialPardonForm &operator=(const PresidentialPardonForm &);
+		PresidentialPardonForm(const std::string &);
+		/*getters*/
+		std::string getTarget(void) const;
+		
+		/*member functions*/
+		void	presidentialPardonAction(void) const;
+		void execute(Bureaucrat const & executor) const;
 };
 
 #endif
