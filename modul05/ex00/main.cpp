afdb43e9834c2002422735757402c31d1e2c6d07
TakrayoutMohamed <takrayoutm@gmail.com>
2024-02-17 10:30:08 +0100

    update ex00 in 05

--- modul05/ex00/main.cpp
+++ modul05/ex00/main.cpp
@@ -4,14 +4,15 @@ int main(void)
 {
 	try
 	{
-		Bureaucrat Bloc0;
+		Bureaucrat B0("alvares", 1);
+		std::cout << "Bureaucrat named "<< B0.getName() << " has grade " <<B0.getGrade() << std::endl;
 		Bureaucrat Bloc1("Governor", 145);
 		std::cout << Bloc1;
 		Bloc1.decrementGrade();
 		std::cout << Bloc1;
 		Bloc1.incrementGrade();
 		std::cout << Bloc1;
-		Bloc0.decrementGrade();
+		B0.decrementGrade();
 	}
 	catch(std::exception &gradeExceptions)
 	{
