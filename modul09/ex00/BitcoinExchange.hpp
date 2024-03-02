3a555e8c080b20d116dfa4d1a5bd43185c6464eb
TakrayoutMohamed <takrayoutm@gmail.com>
2024-03-02 20:43:21 +0100

    add more member and member function to the header file

--- modul09/ex00/BitcoinExchange.hpp
+++ modul09/ex00/BitcoinExchange.hpp
@@ -1,6 +1,9 @@
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 # include <iostream>
+# include <string>
+# include <istringstream>
+# include <sstream>
 # include <algorithm>
 # include <queue>
 # include <pair>
@@ -9,9 +12,12 @@ class BitcoinExchange
 {
 	private:
 
-		std::ostream								currentFile; //stream to read from the file
-		std::string 								line; //used to store the data extracted by getline 
+		std::ostream								_currentFile; //stream to read from the file
+		std::string 								_line; //used to store the data extracted by getline 
 		std::queue<std::pair<std::string, int> >	_data; // the data from the file will be stored here as pair (string , int)
+		std::string									_date;
+		std::string									_exchangeValue;
+		std::istringstream							_stringStream;
 	public:
 		BitcoinExchange(/* args */);
 		BitcoinExchange(const BitcoinExchange &obj);
@@ -20,9 +26,15 @@ class BitcoinExchange
 
 	private :/*member function*/
 		void				openFile(const char *file);
-		const std::string	&readline();
-		bool				checkFirstLine() const;
-		
+		const std::string	&readline(char delim);
+		// if first line return false the programe should quite
+		bool				checkFirstLine(const std::string &firstLine) const;
+		// if return false set to the string "Error bad input"
+		bool				checkOtherLines(const std::string &lineData) const;
+		std::istringstream	&pushLineToStream(const std::string &line);
+		void				spliteLineByPipe();
+
+
 
 		
 };
